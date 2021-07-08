9721c21da212ea14c345a4b1ed4745e6bbf2f144
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-07-08 12:38:09 -0700

    [Initialize] Built initial express server with Hello World.

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT);
+console.log(`Running on http://localhost:${PORT}`);
