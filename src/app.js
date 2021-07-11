d66574ad08eefe88c0b0deeb8a128e9110a28523
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-07-11 15:10:16 -0700

    [update] app.js
    Included common middleware for security, logging, cors, and other common API requirements

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 // Requirements
 const express = require('express')
+const helmet = require('helmet') // Security and HTTP header middleware
+const cors = require('cors') // Handling CORS for accessible APIs
+const morgan = require('morgan') // Request logging
 
 // App
 const app = express()
 
 // Middleware
+app.use(helmet())
+app.use(cors())
+app.use(morgan('common'))
 
 // Routes
 require('./routes/index.js')(app)
