d723127c0b296d03b7ab81f501fd49fd428beebe
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-07-12 16:03:14 -0700

    [update] app.test.js
    Converted from jest/supertest to mocha/chai for better support with express apps

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,9 +1,15 @@
-const request = require('supertest')
+const chai = require('chai')
+const assert = chai.assert
+const chaiHttp = require('chai-http')
 const app = require('../app')
+
+// Configure chai
+chai.use(chaiHttp)
+chai.should()
 
 describe('Test app.js', () => {
   it('Should respond to the GET method', async () => {
-    const response = await request(app).get('/')
-    expect(response.statusCode).toBe(200)
+    const response = await chai.request(app).get('/')
+    assert.strictEqual(response.statusCode, 200, 'Status was success')
   })
 })
